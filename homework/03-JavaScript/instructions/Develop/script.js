cdb3b1f18e69e0fbbb428c525bb9efdcd50630c2
leanndemetro <leanndemetro@gmail.com>
2020-09-10 22:06:44 -0400

    JS update

--- homework/03-JavaScript/instructions/Develop/script.js
+++ homework/03-JavaScript/instructions/Develop/script.js
@@ -1,4 +1,10 @@
-// Assignment Code
+// variable arrays 
+var upCase 
+var lowCase 
+var specChar 
+var num 
+
+// Assignment Code-connects variable to button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -7,8 +13,41 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// creates a function that's return is what shows up in the password text box on the page- so logic should go here
+function generatePassword() {
+
+  // this will appear as a prompt box when generate password button is clicked
+  var charCount = prompt("Choose a password length from 8-128 characters");
+  var upperCase = prompt("Include uppercase characters?");
+  var lowerCase = prompt("Include lowercase characters?");
+  var specialChar = prompt("Include special characters?");
+  var numbers = prompt("Include numbers?");
+
+  if (charCount.length > 8 || charCount.length < 128) 
+  
+   
+
+  if (upperCase == "yes") {
+
+  }
+
+  if (lowerCase == "yes") {
+
+  }
+
+  if (specialChar == "yes") {
+
+  }
+
+  if (numbers == "yes") {
+
+  }
+
+}
+
+
